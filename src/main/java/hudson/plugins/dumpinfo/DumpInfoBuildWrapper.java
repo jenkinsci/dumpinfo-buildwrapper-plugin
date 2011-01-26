7e89674632cad921d786d31d28dad612314ffa20
Jesse Farinacci <jieryn@gmail.com>
2011-01-26 09:46:43 -0500

    refactoring some code out of the plugin

--- src/main/java/hudson/plugins/dumpinfo/DumpInfoBuildWrapper.java
+++ src/main/java/hudson/plugins/dumpinfo/DumpInfoBuildWrapper.java
@@ -26,20 +26,16 @@ package hudson.plugins.dumpinfo;
 
 import hudson.Extension;
 import hudson.Launcher;
-import hudson.PluginWrapper;
 import hudson.PluginManager;
 import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.Computer;
 import hudson.model.Hudson;
-import hudson.model.JDK;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
 
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.List;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 
@@ -70,160 +66,6 @@ public final class DumpInfoBuildWrapper extends BuildWrapper
   }
 
   /**
-   * Dump the Hudson slave computer information to the job log.
-   * 
-   * @param logger
-   *          the AbstractBuild logger
-   * @param computers
-   *          the list of slave computers
-   */
-  private static final void dumpComputers(final PrintStream logger,
-      final Computer[] computers)
-  {
-    if (logger == null)
-    {
-      return;
-    }
-
-    if (computers == null)
-    {
-      return;
-    }
-
-    // ---
-
-    for (final Computer computer : computers)
-    {
-      if (computer.isOnline())
-      {
-        logger.println(Messages.DumpInfo_Computer_Online(
-            computer.getDisplayName(), computer.getNumExecutors(),
-            getRootUrl(computer)));
-      }
-
-      else
-      {
-        logger.println(Messages.DumpInfo_Computer_Offline(
-            computer.getDisplayName(), computer.getNumExecutors(),
-            getRootUrl(computer)));
-      }
-    }
-  }
-
-  /**
-   * Dump the Hudson instance information to the job log.
-   * 
-   * @param logger
-   *          the AbstractBuild logger
-   */
-  private static void dumpHudson(final PrintStream logger)
-  {
-    if (logger == null)
-    {
-      return;
-    }
-
-    // ---
-
-    final Hudson hudson = Hudson.getInstance();
-    logger.println(Messages.DumpInfo_Hudson(hudson.getDisplayName(),
-        Hudson.getVersion()));
-  }
-
-  /**
-   * Dump the Hudson JDK information to the job log.
-   * 
-   * @param logger
-   *          the AbstractBuild logger
-   * @param jdks
-   *          the list of JDK tools
-   */
-  private static final void dumpJdks(final PrintStream logger,
-      final List<JDK> jdks)
-  {
-    if (logger == null)
-    {
-      return;
-    }
-
-    if (jdks == null)
-    {
-      return;
-    }
-
-    // ---
-
-    for (final JDK jdk : jdks)
-    {
-      logger.println(Messages.DumpInfo_Tool_JDK(jdk.getName(), jdk.getHome()));
-    }
-  }
-
-  /**
-   * Dump the Hudson plugin information to the job log.
-   * 
-   * @param logger
-   *          the AbstractBuild logger
-   * @param plugins
-   *          the list of plugins
-   */
-  private static final void dumpPlugins(final PrintStream logger,
-      final List<PluginWrapper> plugins)
-  {
-    if (logger == null)
-    {
-      return;
-    }
-
-    if (plugins == null)
-    {
-      return;
-    }
-
-    // ---
-
-    for (final PluginWrapper plugin : plugins)
-    {
-      logger.println(Messages.DumpInfo_Plugin(plugin.getLongName(),
-          plugin.getVersion(), plugin.getUrl()));
-    }
-  }
-
-  /**
-   * Dump the Hudson plugin information to the job log.
-   * 
-   * @param logger
-   *          the job logger
-   * @param pluginManager
-   *          the plugin manager
-   */
-  private static final void dumpPlugins(final PrintStream logger,
-      final PluginManager pluginManager)
-  {
-    if (pluginManager == null)
-    {
-      return;
-    }
-
-    // ---
-
-    dumpPlugins(logger, pluginManager.getPlugins());
-  }
-
-  /**
-   * Create a fully qualified URL string to reach a particular Computer
-   * definition on the master.
-   * 
-   * @param computer
-   *          the computer to create the URL string for
-   * @return the URL string
-   */
-  private static final String getRootUrl(final Computer computer)
-  {
-    return Hudson.getInstance().getRootUrl() + "/" + computer.getUrl();
-  }
-
-  /**
    * Whether or not to dump information about Hudson slave computers.
    * 
    * @see Hudson#getComputers()
@@ -249,6 +91,8 @@ public final class DumpInfoBuildWrapper extends BuildWrapper
   public DumpInfoBuildWrapper(final boolean dumpComputers,
       final boolean dumpJdks, final boolean dumpPlugins)
   {
+    super();
+
     this.dumpComputers = dumpComputers;
     this.dumpJdks = dumpJdks;
     this.dumpPlugins = dumpPlugins;
@@ -295,21 +139,21 @@ public final class DumpInfoBuildWrapper extends BuildWrapper
 
     // ---
 
-    dumpHudson(logger);
+    HudsonUtils.dumpHudson(logger);
 
     if (dumpComputers)
     {
-      dumpComputers(logger, hudson.getComputers());
+      HudsonUtils.dumpComputers(logger, hudson.getComputers());
     }
 
     if (dumpJdks)
     {
-      dumpJdks(logger, hudson.getJDKs());
+      HudsonUtils.dumpJdks(logger, hudson.getJDKs());
     }
 
     if (dumpPlugins)
     {
-      dumpPlugins(logger, hudson.getPluginManager());
+      HudsonUtils.dumpPlugins(logger, hudson.getPluginManager());
     }
 
     // ---
